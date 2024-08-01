c4c5c3d6deefe30243064349bad55d9a5f281f3c
kennardliong <kennardliong1@gmail.com>
2024-08-01 15:41:59 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const BASE_URL = 'https://frag-predict-final.onrender.com'
+
 document.getElementById('inputForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
@@ -12,7 +14,7 @@ document.getElementById('inputForm').addEventListener('submit', function(event) 
 
     async function fetch2DStructure(smiles, imgElement) {
         try {
-            const response = await fetch('http://localhost:5000/get_2d_structure', {
+            const response = await fetch('${BASE_URL}/get_2d_structure', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -29,7 +31,7 @@ document.getElementById('inputForm').addEventListener('submit', function(event) 
 
     async function fetch3DStructure(smiles, viewerElement, downloadElement, filename) {
         try {
-            const response = await fetch('http://localhost:5000/get_3d_structure', {
+            const response = await fetch('${BASE_URL}/get_3d_structure', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -51,7 +53,7 @@ document.getElementById('inputForm').addEventListener('submit', function(event) 
             downloadElement.style.display = 'block';
             downloadElement.onclick = async function() {
                 try {
-                    const response = await fetch('http://localhost:5000/download_pdb', {
+                    const response = await fetch('${BASE_URL}/download_pdb', {
                         method: 'POST',
                         headers: {
                             'Content-Type': 'application/json'
@@ -76,7 +78,7 @@ document.getElementById('inputForm').addEventListener('submit', function(event) 
 
     async function predictFragment(smiles, suffix) {
         try {
-            const response = await fetch('http://localhost:5000/predict_fragment', {
+            const response = await fetch('${BASE_URL}/predict_fragment', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -110,7 +112,7 @@ document.getElementById('inputForm').addEventListener('submit', function(event) 
 
     async function fetchScore(smiles) {
         try {
-            const response = await fetch('http://localhost:5000/score', {
+            const response = await fetch('${BASE_URL}/score', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -128,7 +130,7 @@ document.getElementById('inputForm').addEventListener('submit', function(event) 
     async function combineFragments(smiles1, smiles2) {
 
         try {
-            const response = await fetch('http://localhost:5000/combine', {
+            const response = await fetch('${BASE_URL}/combine', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
