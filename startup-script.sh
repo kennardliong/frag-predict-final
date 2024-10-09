8e91e7d06d17f7b38da8f7a77b413912b94dd9b4
williammtan <william@tan.id>
2024-10-09 19:20:23 +0700

    update startup script

--- startup-script.sh
+++ startup-script.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-apt update
-apt install git-lfs
-git lfs install
+wget https://github.com/git-lfs/git-lfs/releases/download/v3.5.1/git-lfs-linux-amd64-v3.5.1.tar.gz
+tar xvf git-lfs-linux-amd64-v3.5.1.tar.gz
+
+cd git-lfs-3.5.1/
+./install.sh
+
+cd ..
 
 git lfs fetch --all
 git lfs checkout
